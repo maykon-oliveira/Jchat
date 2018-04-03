a5b5af460eed4e2c553188784b24dc1fb3c8d77e
Cristiane Silva de Andrade <cristiane.a@academico.ifrn.edu.br>
2018-04-03 16:31:11 -0300

    inicio da parte grafica

--- client-side/src/ifrn/edu/jchat/TelaPrincipalController.java
+++ client-side/src/ifrn/edu/jchat/TelaPrincipalController.java
@@ -2,13 +2,22 @@ package ifrn.edu.jchat;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author Maykon Oliveira
  */
 public class TelaPrincipalController implements Initializable {
+
+    @FXML private TextField fieldCaixaMensagem;
+    @FXML private TextField areaDeMensagem;
+    @FXML
+    private Button buttonSair;
     
     /*  ======= TODO =======
         Novo objeto cliente;
@@ -25,6 +34,13 @@ public class TelaPrincipalController implements Initializable {
     /* ===== TODO ======
         Método escreverMensagemTela(Mensagem mensagem);
         Método encapsularMensagem(String texto);
-        Método enviarMensagem(Mensagem mensagem);
     */
+
+    @FXML
+    private void handleEnviarMensagem(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleSair(ActionEvent event) {
+    }
 }
